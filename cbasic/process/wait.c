9b2e3fee7d170fba181d93125619e675088b0da8
Ricardo Campos <ricard_halo@live.com.mx>
2022-08-15 17:18:48 -0500

    process using fork and wait

---
+++ cbasic/process/wait.c
@@ -0,0 +1,24 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main() {
+    int result = fork();
+
+    if (result < 0) {
+        printf("Llamada a fork ha fallado\n");
+        exit(-1);
+    }
+
+    if (result != 0) {
+        wait(NULL);
+        printf("Salida ps:\n");
+        execlp("ps", "ps");
+    } else {
+        printf("Salida ls:\n");
+        execlp("ls", "ls");
+        printf("Ha habido un error...\n");
+    }
+}
